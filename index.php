210aadb47a13631bf67b5eab506ac552bbe49b78
AgungDwiW <agung.dwi.temp@gmail.com>
2020-02-07 22:45:53 +0700

    side bar

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 include ("sidebar_top.html");
 ?>
+<?php
+include ("sidebar_top2.html");
+?>
 
 <h2>Collapsible Sidebar Using Bootstrap 4</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -23,3 +26,4 @@ include ("sidebar_top.html");
 <?php
 include ("sidebar_botom.html");
 ?>
+</html>
---
+++ profile.php
@@ -0,0 +1,51 @@
+<?php
+include ("sidebar_top.html");
+?>
+
+<link href="profile.css" rel="stylesheet" id="bootstrap-css">
+<!------ Include the above in your HEAD tag ---------->
+
+
+<?php
+include ("sidebar_top2.html");
+?>
+<div class="container profile">
+	<div class="row">
+        <div class="row">
+            
+            <div class="col-sm-4 text-center">
+                <figure>
+                    <img src="profile.png" alt="" class="img-circle img-fluid">
+                </figure>
+            </div>
+            <div class="col-sm-8">
+                <h2>Jack Jackerman</h2>
+                <p><strong>Class : </strong> IT </p>
+                <p><strong>Buddy : </strong> Budi Budiman </p>
+                <p><strong>Email : </strong> jackman@jack.com </p>
+            </div>             
+        </div>          
+
+        <div class="row col-sm-12 text-center divider">
+            <div class=" col-sm-4 emphasis">
+                <h2><strong> 1 </strong></h2>                    
+                <p><small>Main Quest Done</small></p>
+                <!-- <button class="btn btn-success btn-block"><span class="fa fa-plus-circle"></span> Follow </button> -->
+            </div>
+            <div class=" col-sm-4 emphasis">
+                <h2><strong>0</strong></h2>                    
+                <p><small>Sub Quest Done</small></p>
+                <!-- <button class="btn btn-info btn-block"><span class="fa fa-user"></span> View Profile </button> -->
+            </div>
+            <div class=" col-sm-4 emphasis">
+                <h2><strong>1</strong></h2>                    
+                <p><small>Stage</small></p>
+            </div>
+        </div>
+	
+	</div>
+</div>
+
+<?php
+include ("sidebar_botom.html");
+?>
