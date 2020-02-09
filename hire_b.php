975b79177546ee65c8d53a5e654b160a0aa2ca2f
AgungDwiW <agung.dwi.temp@gmail.com>
2020-02-09 07:42:17 +0700

    finis

--- hire_b.php
+++ hire_b.php
@@ -3,7 +3,7 @@
 require ("dbcon.php");
 
 
-$sql = "insert into user(name, username, password, email, status, class, credit) values('".$_POST['name']."', '".$_POST['username']."', '".$_POST['password']."', '".$_POST['email']."', 1, '".$_POST['class']."', 100)";
+$sql = "insert into user(name, username, password, email, status, class, credit) values('".$_POST['name']."', '".$_POST['username']."', '".$_POST['password']."', '".$_POST['email']."', 1, '".$_POST['class']."', 10000)";
 $result = $conn->query($sql);
 
 header("Location: buddy_list.php");
